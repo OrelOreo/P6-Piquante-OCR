2ebbb02f4af0f3b4cc31f7e4407a341b2bc082b2
OrelOreo <oreloreopro@gmail.com>
2023-03-31 16:39:49 +0200

    correction mime_type

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const MIME_TYPES = {
     'image/jpg' : 'jpg',
-    'image/jpeg' : 'jpg',
+    'image/jpeg' : 'jpeg',
     'image/png' : 'png'
 }
 
@@ -12,7 +12,6 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, callback) => {
         const name = file.originalname.replace(' ', '_').replace('.jpg', '')
-        console.log("name", name)
         const extension = MIME_TYPES[file.mimetype]
         callback(null, name + Date.now() + '.' + extension)
     }
